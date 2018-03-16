8b083bf1e9533350e63a27ce7db1b19c2301a211
gubanotorious <6355333+gubanotorious@users.noreply.github.com>
2018-03-16 02:35:59 -0400

    - Created Neo Web Debugger Project / MVC Framework - SignalR configured to handle server <-> client for realtime communication - Added Semantic UI framework - Created web based compiler proof of concept to test event logging and compilation

---
+++ NEO-WebDebugger/Controllers/CompilerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+using Neo.Debugger.Core.Models;
+using Neo.Debugger.Core.Utils;
+
+namespace Neo.WebDebugger.Controllers
+{
+    public class CompilerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Compile(string source)
+        {
+            //Use settings from the My Documents folder, in a hosted / multi-tenant environment, this will have to change.  This works for local machine for now
+            var settings = new Settings(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+
+            Compiler compiler = new Compiler(settings);
+            compiler.SendToLog += Compiler_SendToLog;
+
+            Directory.CreateDirectory(settings.path);
+            var fileName = Path.Combine(settings.path, "DebugContract.cs");
+
+            bool success = compiler.CompileContract(source, fileName);
+
+            if (success)
+            {
+                compiler.Log("Contract compiled successfully.");
+            }
+
+            return Json(success);
+        }
+
+        private void Compiler_SendToLog(object sender, Debugger.Core.Models.CompilerLogEventArgs e)
+        {
+            GlobalHost.ConnectionManager.GetHubContext<DebuggerHub>().Clients.All.logEventMessage(e.Message);
+        }
+    }
+}
---
+++ NEO-WebDebugger/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Neo.WebDebugger.MvcApplication" Language="C#" %>
