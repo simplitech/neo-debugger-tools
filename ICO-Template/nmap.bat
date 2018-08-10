192c55d8f43e727ec08101a69c7a2c27ffcbf84d
Sergio Flores <relfos@gmail.com>
2018-08-10 19:29:44 +0100

    Start of Debugger remake

--- ICO-Template/nmap.bat
+++
@@ -1,4 +0,0 @@
-
-set PATH="D:\repos\neo-debugger-tools\NEO-Compiler\bin\Debug";%PATH%
-neon.exe ICOContract.dll
-pause
--- NEO-WebDebugger/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Neo.WebDebugger.MvcApplication" Language="C#" %>
