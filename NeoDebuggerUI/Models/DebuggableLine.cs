58213ed1de7e2cfed79261bb6b0fe6fc4a52f96c
Ricardo Prado <ricardo.prado@simpli.com.br>
2019-01-22 16:29:37 -0200

    Menu (UI)

---
+++ NeoDebuggerUI/Models/DebuggableLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeoDebuggerUI.Models
+{
+	public class DebuggableLine
+	{
+		public int LineNumber { get; set; }
+		public bool ActiveBreakpoint { get; set; }
+		public string LineContent { get; set; }
+
+		public DebuggableLine(int lineNumber, bool activeBreakpoint, string lineContent)
+		{
+			LineNumber = lineNumber;
+			ActiveBreakpoint = activeBreakpoint;
+			LineContent = lineContent;
+		}
+	}
+}
--- NeoDebuggerUI/ViewModels/MainWindowViewModel.cs
+++ NeoDebuggerUI/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using NeoDebuggerUI.Models;
 
 namespace NeoDebuggerUI.ViewModels
 {
 	public class MainWindowViewModel : ViewModelBase
 	{
-		public string Greeting => "Hello World!";
+		public string[] FileNames => new string[] { "File1.cs", "File2.cs", "File3.cs", "File4.cs" };
+		public List<DebuggableLine> FileContent { get; set; }
+
+		public MainWindowViewModel()
+		{
+			FileContent = new List<DebuggableLine>();
+			for (int i = 0; i < 2; i++)
+			{
+				var line = new DebuggableLine(i, false, "Line " + i);
+				FileContent.Add(line);
+			}
+		}
 	}
 }
