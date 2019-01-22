bbb5a2061986e04004c7c88814e74289a3d2188b
Ricardo Prado <ricardo.prado@simpli.com.br>
2019-01-22 18:57:58 -0200

    Adding Avalonia Edit and dependencies

--- NeoDebuggerUI/ViewModels/MainWindowViewModel.cs
+++ NeoDebuggerUI/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Neo.Debugger.Core.Models;
