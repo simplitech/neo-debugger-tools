85d08e4abdcbcc0fb76227db3e0cf5c1e4ababaf
Mirella de Medeiros <mirellamedeiros.09@hotmail.com>
2019-03-15 14:29:10 -0300

    Show tooltip

--- NeoDebuggerUI/Views/MainWindow.xaml.cs
+++ NeoDebuggerUI/Views/MainWindow.xaml.cs
@@ -30,13 +30,15 @@ namespace NeoDebuggerUI.Views
             _textEditor.Options.ConvertTabsToSpaces = true;
             _textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinition("C#");
             _textEditor.TextArea.IndentationStrategy = new AvaloniaEdit.Indentation.CSharp.CSharpIndentationStrategy(_textEditor.Options);
+            _textEditor.PointerHover += (o, e) => SetTip(e.GetPosition(_textEditor));
+            _textEditor.PointerHoverStopped += (o, e) => ToolTip.SetIsOpen(_textEditor, false);
 
             MenuItem newCSharp = this.FindControl<MenuItem>("MenuItemNewCSharp");
             newCSharp.Click += async (o, e) => { await NewCSharpFile(); };
 
             this.ViewModel.EvtFileChanged += (fileName) => LoadFile(fileName);
             this.ViewModel.EvtFileToCompileChanged += () => ViewModel.SaveCurrentFileWithContent(_textEditor.Text);
-            
+
         }
 
         public async Task NewCSharpFile()
@@ -60,11 +62,21 @@ namespace NeoDebuggerUI.Views
             _textEditor.Load(fs);
         }
 
+        public void SetTip(Point mousePosition)
+        {
+            var textPosition = _textEditor.GetPositionFromPoint(mousePosition);
+
+            var lineIndex = textPosition?.Line;
+            var columnIndex = textPosition?.Column;
+
+            ToolTip.SetTip(_textEditor, $"This is just a test on position {lineIndex}, {columnIndex}");
+            ToolTip.SetIsOpen(_textEditor, true);
+        }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
-        
+
     }
 }
