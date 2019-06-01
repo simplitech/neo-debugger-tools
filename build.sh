d8021a3ba5bccbf718e813619a055cbd05a7bf68
Ricardo Prado <ricardo.prado@simpli.com.br>
2019-06-01 02:28:08 -0300

    Travis 1

---
+++ build.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+set -ev
+git clone https://github.com/AvaloniaUI/AvaloniaEdit.git
+dotnet remove NEODebuggerUI package Avalonia.AvaloniaEdit 
+dotnet add ./NEODebuggerUI/NEODebuggerUI.csproj reference AvaloniaEdit/src/AvaloniaEdit/AvaloniaEdit.csproj 
+dotnet restore
+dotnet build -c Release
