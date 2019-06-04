82c84c31fa363b7be3f095387b371687b47b196b
Ricardo Prado <ricardo.prado@simpli.com.br>
2019-06-04 16:43:50 -0300

    Friendly zip name

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r win-x64 -f netco
 dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r linux-x64 -f netcoreapp2.2
 BUILD_DIR=$(pwd)
 cd NEODebuggerUI/bin/release/netcoreapp2.2/osx-x64/publish
-zip -r $BUILD_DIR/osx-x64.zip ./*
+zip -r $BUILD_DIR/neo-debugger-osx-x64.zip ./*
 cd -
 cd NEODebuggerUI/bin/release/netcoreapp2.2/win-x64/publish
-zip -r $BUILD_DIR/win-x64.zip ./*
+zip -r $BUILD_DIR/neo-debugger-win-x64.zip ./*
 cd -
 cd NEODebuggerUI/bin/release/netcoreapp2.2/linux-x64/publish
-zip -r $BUILD_DIR/linux-x64.zip ./*
+zip -r $BUILD_DIR/neo-debugger-linux-x64.zip ./*
 cd -
