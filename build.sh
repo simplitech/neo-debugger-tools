74e0d65e16336c759fbb075fce431de595d83ae7
Ricardo Prado <ricardo.prado@simpli.com.br>
2019-06-04 16:23:30 -0300

    Improving zip structure

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ dotnet restore
 dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r osx-x64 -f netcoreapp2.2
 dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r win-x64 -f netcoreapp2.2
 dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r linux-x64 -f netcoreapp2.2
-zip -r osx-x64-$1.zip NEODebuggerUI/bin/release/netcoreapp2.2/osx-x64/publish
-zip -r win-x64-$1.zip NEODebuggerUI/bin/release/netcoreapp2.2/win-x64/publish
-zip -r linux-x64-$1.zip NEODebuggerUI/bin/release/netcoreapp2.2/linux-x64/publish
+cd NEODebuggerUI/bin/release/netcoreapp2.2/osx-x64/publish
+zip -r osx-x64.zip ./*
+cd -
+cd NEODebuggerUI/bin/release/netcoreapp2.2/win-x64/publish
+zip -r win-x64.zip ./*
+cd -
+cd NEODebuggerUI/bin/release/netcoreapp2.2/linux-x64/publish
+zip -r linux-x64.zip ./*
+cd -
