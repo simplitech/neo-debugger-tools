510af1083d1f794ae4a605d4661c83f0d8c18b04
Ricardo Prado <ricardo.prado@simpli.com.br>
2019-06-04 16:33:51 -0300

    zip folder

--- build.sh
+++ build.sh
@@ -4,12 +4,13 @@ dotnet restore
 dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r osx-x64 -f netcoreapp2.2
 dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r win-x64 -f netcoreapp2.2
 dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r linux-x64 -f netcoreapp2.2
+BUILD_DIR=$(pwd)
 cd NEODebuggerUI/bin/release/netcoreapp2.2/osx-x64/publish
-zip -r osx-x64.zip ./*
+zip -r $BUILD_DIR/osx-x64.zip ./*
 cd -
 cd NEODebuggerUI/bin/release/netcoreapp2.2/win-x64/publish
-zip -r win-x64.zip ./*
+zip -r $BUILD_DIR/win-x64.zip ./*
 cd -
 cd NEODebuggerUI/bin/release/netcoreapp2.2/linux-x64/publish
-zip -r linux-x64.zip ./*
+zip -r $BUILD_DIR/linux-x64.zip ./*
 cd -
