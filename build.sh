bd818f2e37fced7a429313522c9456d4783eacc4
Ricardo Prado <ricardo.prado@simpli.com.br>
2019-06-04 00:22:03 -0300

    Building project only Fix zip in docker build

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-﻿#!/bin/bash
+﻿#!/bin/sh
 set -ev
 dotnet restore
-dotnet publish -c release -r osx-x64 -f netcoreapp2.2
-dotnet publish -c release -r win-x64 -f netcoreapp2.2
-dotnet publish -c release -r linux-x64 -f netcoreapp2.2
-zip -r osx-x64-$0.zip NEODebuggerUI/bin/Release/netcoreapp2.2/osx-x64/publish
-zip -r win-x64-$0.zip NEODebuggerUI/bin/Release/netcoreapp2.2/win-x64/publish
-zip -r linux-x64-$0.zip NEODebuggerUI/bin/Release/netcoreapp2.2/linux-x64/publish
+dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r osx-x64 -f netcoreapp2.2
+dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r win-x64 -f netcoreapp2.2
+dotnet publish NEODebuggerUI/NEODebuggerUI.csproj -c release -r linux-x64 -f netcoreapp2.2
+zip -r osx-x64-$1.zip NEODebuggerUI/bin/release/netcoreapp2.2/osx-x64/publish
+zip -r win-x64-$1.zip NEODebuggerUI/bin/release/netcoreapp2.2/win-x64/publish
+zip -r linux-x64-$1.zip NEODebuggerUI/bin/release/netcoreapp2.2/linux-x64/publish
