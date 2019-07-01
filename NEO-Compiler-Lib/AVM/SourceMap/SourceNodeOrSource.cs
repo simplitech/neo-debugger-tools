be57f3c384b29e4dad819e8d204956a459b85f03
gil <gil.bueno@simpli.com.br>
2019-07-01 19:27:25 -0300

    Source Map first experimenting Draft

---
+++ NEO-Compiler-Lib/AVM/SourceMap/SourceNodeOrSource.cs
@@ -0,0 +1,18 @@
+namespace NEO.Compiler.AVM.SourceMap
+{
+    public class SourceNodeOrSource
+    {
+        public SourceNode SourceNode { get; set; }
+        public string Source { get; set; }
+
+        public SourceNodeOrSource(SourceNode sourceNode)
+        {
+            SourceNode = sourceNode;
+        }
+
+        public SourceNodeOrSource(string source)
+        {
+            Source = source;
+        }
+    }
+}
